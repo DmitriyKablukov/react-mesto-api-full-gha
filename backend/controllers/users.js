ff77af3ad7862ee6b5b3eb0d7f5143b36092e399
DmitriyKablukov <dima-kablukov@ya.ru>
2024-01-08 12:45:41 +0300

    Итог после ревью 1
    (Исправления после автотестов)

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,5 +1,4 @@
 /* eslint-disable consistent-return */
-const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
